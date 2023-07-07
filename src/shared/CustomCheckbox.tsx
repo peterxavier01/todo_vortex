26aa7c7f7ad8339eba2f03b920fb7375b72edc48
Peter Uadiale <peteuadiale1@gmail.com>
2023-07-07 09:09:45 +0100

    update CustomCheckbox.tsx

--- src/shared/CustomCheckbox.tsx
+++ src/shared/CustomCheckbox.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import "../styles/CustomCheckbox.scss";
 import CheckIcon from "../assets/icon-check.svg";
 
@@ -7,9 +8,24 @@ interface Props {
 }
 
 const CustomCheckbox: React.FC<Props> = ({ handleToggle, checked }) => {
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const handleMouseEnter = () => {
+    if (!checked) {
+      divRef.current?.classList.add("hover");
+    }
+  };
+
+  const handleMouseLeave = () => {
+    divRef.current?.classList.remove("hover");
+  };
+
   return (
     <div
+      ref={divRef}
       className={`custom-checkbox ${checked ? "checked" : ""}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       onClick={handleToggle}
     >
       {checked && <img src={CheckIcon} alt="check-icon" />}
